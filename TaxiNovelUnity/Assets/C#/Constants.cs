9acec1110bf273bb2d9d1a201a2894ec39188afc
Kouki Shimoya <shimoyameiji@gmail.com>
2019-12-03 16:08:23 +0900

    簡単な会話を追加

---
+++ TaxiNovelUnity/Assets/C#/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Constants
+{
+    public class Tag
+    {
+        public const string Player = "Player";
+    }
+
+}
---
+++ TaxiNovelUnity/Assets/C#/FungusMessageSend.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Constants;
+
+public class FungusMessageSend : MonoBehaviour
+{
+    [SerializeField] private Fungus.Flowchart flowchart;
+    [SerializeField] private string message;
+    
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag(Tag.Player))
+        {
+            flowchart.SendFungusMessage(message);
+        }
+    }
+}
