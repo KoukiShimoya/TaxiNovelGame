fb5b3b55ec03d5f6b2c0138fc749b3d30e48d505
Kouki Shimoya <shimoyameiji@gmail.com>
2019-12-17 18:19:52 +0900

    車の挙動をTransfrom.Rotate基準に調整

--- TaxiNovelUnity/Assets/C#/CarMove.cs
+++ TaxiNovelUnity/Assets/C#/CarMove.cs
@@ -13,21 +13,25 @@ public class CarMove : MonoBehaviour
     /// </summary>
     [SerializeField] private float steering;
  
-    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Rigidbody2D rigidbody2d;
 
     /// <summary>
     /// 何度ズレていたら真正面や真横に自動的に修正するか
     /// </summary>
     [SerializeField] private float autoFixInclinationThresould;
     
-    private float currentSpeed;
+    private float currentSpeed; //現在速度メータなどが必要となった時に使う
+
+    private KeyInput keyInput;
  
     private void Start()
     {
-        if (rb == null)
+        if (rigidbody2d == null)
         {
-            this.rb = GetComponent<Rigidbody2D>();
+            this.rigidbody2d = GetComponent<Rigidbody2D>();
         }
+
+        keyInput = KeyInput.Instance;
     }
  
     private void FixedUpdate()
@@ -37,43 +41,65 @@ public class CarMove : MonoBehaviour
         {
             return;
         }
-        
-        // Get input
-        float h = Input.GetAxis("Horizontal");
-        float v = -Input.GetAxis("Vertical");
+
+        float horizontal = HorizontalButtonPushLevel();
+        float vertical = VerticalButtonPushLevel();
                  
         // Calculate speed from input and acceleration (transform.up is forward)
-        Vector2 speed = transform.up * (v * acceleration);
-        rb.AddForce(speed);
+        Vector2 speed = transform.up * (vertical * acceleration);
+        rigidbody2d.AddForce(speed);
         
-        
-        // Create car rotation
-        float direction = Vector2.Dot(rb.velocity, rb.GetRelativeVector(Vector2.up));
-        if (direction >= 0f)
+        transform.Rotate(0, 0, horizontal * steering);
+ 
+        // Force max speed limit
+        if (rigidbody2d.velocity.magnitude > maxSpeed)
         {
-            rb.rotation += h * steering * (rb.velocity.magnitude / maxSpeed);
+            rigidbody2d.velocity = rigidbody2d.velocity.normalized * maxSpeed;
+        }
+        
+        AutoFixInclination(horizontal, vertical);
+        
+        currentSpeed = rigidbody2d.velocity.magnitude;
+    }
+
+    private float HorizontalButtonPushLevel()
+    {
+        if (keyInput.GetKeyList.Contains(KeyInput.KEYTYPE.A) && keyInput.GetKeyList.Contains((KeyInput.KEYTYPE.D)))
+        {
+            return 0;
+        }
+        else if (keyInput.GetKeyList.Contains(KeyInput.KEYTYPE.A))
+        {
+            return 1;
+        }
+        else if (keyInput.GetKeyList.Contains(KeyInput.KEYTYPE.D))
+        {
+            return -1;
         }
         else
         {
-            rb.rotation -= h * steering * (rb.velocity.magnitude / maxSpeed);
+            return 0;
         }
-        
- 
-        // Change velocity based on rotation
-        float driftForce = Vector2.Dot(rb.velocity, rb.GetRelativeVector(Vector2.left)) * 2.0f;
-        Vector2 relativeForce = Vector2.right * driftForce;
-        EditorDebug.DrawLine(rb.position, rb.GetRelativePoint(relativeForce), Color.green);
-        rb.AddForce(rb.GetRelativeVector(relativeForce));
- 
-        // Force max speed limit
-        if (rb.velocity.magnitude > maxSpeed)
+    }
+
+    private float VerticalButtonPushLevel()
+    {
+        if (keyInput.GetKeyList.Contains(KeyInput.KEYTYPE.W) && keyInput.GetKeyList.Contains((KeyInput.KEYTYPE.S)))
         {
-            rb.velocity = rb.velocity.normalized * maxSpeed;
+            return 0;
         }
-        
-        AutoFixInclination(h, v);
-        
-        currentSpeed = rb.velocity.magnitude;
+        else if (keyInput.GetKeyList.Contains(KeyInput.KEYTYPE.W))
+        {
+            return -1;
+        }
+        else if (keyInput.GetKeyList.Contains(KeyInput.KEYTYPE.S))
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
     }
 
     /// <summary>
@@ -88,12 +114,15 @@ public class CarMove : MonoBehaviour
             return;
         }
 
+        rigidbody2d.angularVelocity = 0;
+
         float absRotationZ = this.gameObject.transform.eulerAngles.z;
         
         while (absRotationZ < 0f)
         {
             absRotationZ += 360;
         }
+        EditorDebug.Log(absRotationZ);
         
         if (absRotationZ <= autoFixInclinationThresould || absRotationZ >= 360 - autoFixInclinationThresould)
         {
@@ -114,7 +143,7 @@ public class CarMove : MonoBehaviour
         
         void AutoRotate(int z)
         {
-            rb.MoveRotation(Quaternion.Slerp(transform.rotation, Quaternion.Euler(0,0,z) ,0.05f));
+            rigidbody2d.MoveRotation(Quaternion.Slerp(transform.rotation, Quaternion.Euler(0,0,z) ,0.05f));
         }
     }
 }
