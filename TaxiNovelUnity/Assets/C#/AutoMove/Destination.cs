b2a1b820afc61641f1800619574c51f5c855d9ff
KoukiShimoya <shimoyameiji@gmail.com>
2019-12-27 18:03:18 +0900

    プレイヤーの自動運転を追加

---
+++ TaxiNovelUnity/Assets/C#/AutoMove/Destination.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destination
+{
+    public static Vector2 destination = new Vector2(10.5f, 8.5f);
+}
---
+++ TaxiNovelUnity/Assets/C#/AutoMove/DestinationToggleChanged.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Navigation4TilemapNameSpace;
+
+public class DestinationToggleChanged : MonoBehaviour
+{
+    [SerializeField] private Toggle toggle;
+
+    private Navigation4Tilemap navigation4Tilemap;
+
+    private void Start()
+    {
+        toggle = this.gameObject.GetComponent<Toggle>();
+        navigation4Tilemap = Navigation4Tilemap.Instance;
+    }
+
+    public void OnToggleChanged()
+    {
+        if (toggle.isOn)
+        {
+            navigation4Tilemap.StopAllCoroutines();
+            navigation4Tilemap.FindingPath(
+                new Vector2(navigation4Tilemap.player.transform.position.x,
+                    navigation4Tilemap.player.transform.position.y), Destination.destination);
+            navigation4Tilemap.StopFindAndStartCoroutine();
+        }
+        else
+        {
+            navigation4Tilemap.StopAllCoroutines();
+            navigation4Tilemap.InActivePathMarks();
+        }
+    }
+}
