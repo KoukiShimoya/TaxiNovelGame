038f0c7d89f08c7fea8a8f5e2358e78e15ac8425
KoukiShimoya <shimoyameiji@gmail.com>
2019-12-29 19:55:12 +0900

    ゲーム画面内の設定画面の仮版完成

--- TaxiNovelUnity/Assets/C#/SettingCanvas/SettingPanelUniqueOpen.cs
+++ TaxiNovelUnity/Assets/C#/SettingCanvas/SettingPanelUniqueOpen.cs
@@ -5,32 +5,68 @@ using UnityEngine;
 
 public class SettingPanelUniqueOpen : MonoBehaviour
 {
-    [SerializeField] private GameObject languagePanel;
-    [SerializeField] private GameObject howToUsePanel;
+    [SerializeField] private List<ButtonPanelSet> buttonPanelSetList;
     [SerializeField] private GameObject settingCanvas;
+    private WorldStateHolder worldStateHolder;
+    private SettingToStartScene settingToStartScene;
 
     /// <summary>
-    /// 
+    /// 設定画面でボタンを押した際、開かれる画面を一意にする挙動
     /// </summary>
     /// <param name="languageOrHowToUse">languagePanelならtrue, HowToUsePanelならfalse</param>
-    public void ChangeActivePanel(bool languageOrHowToUse)
+    public void ChangeActivePanel(GameObject button)
     {
-        if (languageOrHowToUse)
+        foreach (var buttonPanelSet in buttonPanelSetList)
         {
-            languagePanel.SetActive(true);
-            howToUsePanel.SetActive(false);
+            if (buttonPanelSet.Button == button)
+            {
+                buttonPanelSet.Panel.SetActive(true);
+            }
+            else
+            {
+                buttonPanelSet.Panel.SetActive(false);
+            }
         }
-        else
+    }
+
+    /// <summary>
+    /// 設定画面で閉じるボタンを押したときの挙動
+    /// </summary>
+    public void CloseSettingPanel()
+    {
+        settingCanvas.SetActive(false);
+        
+        foreach (var buttonPanelSet in buttonPanelSetList)
         {
-            languagePanel.SetActive(false);
-            howToUsePanel.SetActive(true);
+            buttonPanelSet.Panel.SetActive(false);
+        }
+        
+        if (worldStateHolder != null)
+        {
+            worldStateHolder.GetSetWorldState = WorldStateHolder.WorldState.Standard;
+        }
+
+        if (settingToStartScene != null)
+        {
+            settingToStartScene.GetReturnStartPanel.SetActive(false);
         }
     }
 
     private void Start()
     {
         settingCanvas.SetActive(false);
-        languagePanel.SetActive(false);
-        howToUsePanel.SetActive(false);
+        foreach (var buttonPanelSet in buttonPanelSetList)
+        {
+            buttonPanelSet.Panel.SetActive(false);
+        }
+        worldStateHolder = WorldStateHolder.Instance;
+        settingToStartScene = SettingToStartScene.Instance;
     }
+}
+
+[System.Serializable]
+public class ButtonPanelSet
+{
+    public GameObject Button;
+    public GameObject Panel;
 }
---
+++ TaxiNovelUnity/Assets/C#/SettingCanvas/SettingToStartScene.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ConstValues;
+
+public class SettingToStartScene : SingletonMonoBehaviour<SettingToStartScene>
+{
+    [SerializeField] private GameObject returnStartPanel;
+
+    public GameObject GetReturnStartPanel
+    {
+        get { return returnStartPanel; }
+    }
+
+    private void Start()
+    {
+        returnStartPanel.SetActive(false);
+    }
+
+    public void OnOpenReturnStartPanelButtonClick()
+    {
+        returnStartPanel.SetActive(true);
+    }
+
+    public void OnReturnSettingPanelButtonClick()
+    {
+        returnStartPanel.SetActive(false);
+    }
+
+    public void OnReturnStartSceneButtonClick()
+    {
+        SceneChange.Instance.NoPostEffectSceneChange(SceneName.StartScene);
+    }
+}
