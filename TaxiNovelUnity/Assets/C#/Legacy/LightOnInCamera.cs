429d52469e65a19bebc901afa497d9e6cce81c5d
KoukiShimoya <shimoyameiji@gmail.com>
2020-02-07 00:19:40 +0900

    影をパネル式に変更

---
+++ TaxiNovelUnity/Assets/C#/Legacy/LightOnInCamera.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightOnInCamera : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    private bool isVisible;
+    private GameObject car;
+
+    private void Start()
+    {
+        spriteRenderer = this.gameObject.GetComponent<SpriteRenderer>();
+        spriteRenderer.enabled = false;
+        car = PlayerStateOwner.Instance.gameObject;
+    }
+
+    private void LateUpdate()
+    {
+        if (Vector2.Distance(car.transform.position, this.gameObject.transform.position) < 15)
+        {
+            if (!spriteRenderer.enabled)
+            {
+                spriteRenderer.enabled = true;
+            }
+        }
+        else
+        {
+            if (spriteRenderer.enabled)
+            {
+                spriteRenderer.enabled = false;
+            }
+        }
+    }
+}
