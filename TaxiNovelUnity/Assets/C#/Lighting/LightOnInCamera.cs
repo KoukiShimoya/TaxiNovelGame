37a40b3a6395bff179d41c0340792c2da972e4c1
KoukiShimoya <shimoyameiji@gmail.com>
2020-02-06 23:02:14 +0900

    影の追加

---
+++ TaxiNovelUnity/Assets/C#/Lighting/LightOnInCamera.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.LWRP;
+
+public class LightOnInCamera : MonoBehaviour
+{
+    private Light2D light2D;
+    private bool isVisible;
+    private GameObject car;
+
+    private void Start()
+    {
+        light2D = this.gameObject.GetComponent<Light2D>();
+        light2D.enabled = false;
+        car = PlayerStateOwner.Instance.gameObject;
+    }
+
+    private void LateUpdate()
+    {
+        if (Vector2.Distance(car.transform.position, this.gameObject.transform.position) < 15)
+        {
+            if (!light2D.isActiveAndEnabled)
+            {
+                light2D.enabled = true;
+            }
+        }
+        else
+        {
+            if (light2D.isActiveAndEnabled)
+            {
+                light2D.enabled = false;
+            }
+        }
+    }
+}
