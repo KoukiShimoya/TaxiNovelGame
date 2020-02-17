b9be3993da24cfcd602e1e3827c0870e77e2a788
KoukiShimoya <shimoyameiji@gmail.com>
2020-02-17 11:59:50 +0900

    警察車のSlerp関数の追加

--- TaxiNovelUnity/Assets/C#/AutoMove/GeneralCarMove.cs
+++ TaxiNovelUnity/Assets/C#/AutoMove/GeneralCarMove.cs
@@ -34,7 +34,10 @@ public class GeneralCarMove : MonoBehaviour
 
             destination = checkPointList[0];
             aimPointNumber = 0;
-            this.gameObject.transform.LookAt2D(destination);
+            
+            Quaternion target = TransformExtensions.LookAt2D(this.gameObject.transform, destination);
+            this.gameObject.transform.rotation = target;
+            //StartCoroutine(SlerpCoroutine(target));
         }
     }
 
@@ -60,10 +63,23 @@ public class GeneralCarMove : MonoBehaviour
 
             destination = checkPointList[aimPointNumber];
             
-            this.gameObject.transform.LookAt2D(destination);
+            Quaternion target = TransformExtensions.LookAt2D(this.gameObject.transform, destination);
+            this.gameObject.transform.rotation = target;
+            //StartCoroutine(SlerpCoroutine(target));
         }
         
         CarMove();
+    }
+
+    private IEnumerator SlerpCoroutine(Quaternion target)
+    {
+        float elapsedTime = 0f;
+        while (elapsedTime < 1f)
+        {
+            this.gameObject.transform.rotation = Quaternion.RotateTowards(this.gameObject.transform.rotation, target, 500f * Time.deltaTime);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
     }
 
     private void CarMove()
