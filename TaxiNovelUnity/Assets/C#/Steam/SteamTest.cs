7ed8ff369da9c83a5e455754ddaf5a62291a4c4c
Kouki Shimoya <shimoyameiji@gmail.com>
2020-02-20 15:25:54 +0900

    Steamno パッケージを入れた

---
+++ TaxiNovelUnity/Assets/C#/Steam/SteamTest.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using Steamworks;
+
+public class SteamTest : MonoBehaviour {
+    void Start() {
+        if(SteamManager.Initialized) {
+            string name = SteamFriends.GetPersonaName();
+            EditorDebug.Log(SteamUtils.GetAppID());
+        }
+    }
+}
