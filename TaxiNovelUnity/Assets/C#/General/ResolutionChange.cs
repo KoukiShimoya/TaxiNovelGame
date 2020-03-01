1facdfd516110392c9569951663c414cf04ae38b
KoukiShimoya <shimoyameiji@gmail.com>
2020-03-02 00:45:55 +0900

    解像度を強制変更

---
+++ TaxiNovelUnity/Assets/C#/General/ResolutionChange.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResolutionChange : MonoBehaviour
+{
+    [SerializeField] private ResolutionStateHolder.ResolutionState resolutionState;
+
+    public void OnValueChanged(bool isOn)
+    {
+        if (!isOn)
+        {
+            return;
+        }
+
+        ResolutionStateHolder.ResolutionStateChange(resolutionState);
+    }
+}
---
+++ TaxiNovelUnity/Assets/C#/General/ResolutionStateHolder.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Kirurobo;
+using UnityEditor;
+using UnityEngine;
+
+public static class ResolutionStateHolder
+{
+    public enum ResolutionState
+    {
+        FullScreen,
+        FullScreen_Windowed,
+        R3840_2160,
+        R2560_1440,
+        R1920_1080,
+        R1600_900,
+        R1366_768,
+        R1280_720
+    }
+
+    private static ResolutionState resolutionState;
+
+    public static void ResolutionStateChange(ResolutionState state)
+    {
+        resolutionState = state;
+        
+        Screen.fullScreen = false;
+        Screen.fullScreenMode = FullScreenMode.Windowed;
+        
+        switch (state)
+        {
+            case ResolutionState.FullScreen:
+                Screen.fullScreen = true;
+                Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+                break;
+                
+            case ResolutionState.FullScreen_Windowed:
+                UniWinApi uniWin = new UniWinApi();
+                var window = UniWinApi.FindWindow();
+                uniWin.SetWindow(window);
+                uniWin.Maximize();
+                uniWin.Dispose();
+                break;
+            
+            case ResolutionState.R3840_2160:
+                Screen.SetResolution(3840, 2160, false);
+                break;
+            
+            case ResolutionState.R2560_1440:
+                Screen.SetResolution(2560, 1440, false);
+                break;
+                
+            case ResolutionStateHolder.ResolutionState.R1920_1080:
+                Screen.SetResolution(1920, 1080, false);
+                break;
+            
+            case ResolutionState.R1600_900:
+                Screen.SetResolution(1600, 900, false);
+                break;
+            
+            case ResolutionState.R1366_768:
+                Screen.SetResolution(1366, 768, false);
+                break;
+            
+            case ResolutionState.R1280_720:
+                Screen.SetResolution(1280, 720, false);
+                break;
+            
+            default:
+                break;
+        }
+    }
+
+    public static void Initialize()
+    {
+        ResolutionStateChange(resolutionState);
+    }
+}
