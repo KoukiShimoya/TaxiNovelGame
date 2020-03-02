c3c0ed124a7a94c180feb75c2621434fad568f26
KoukiShimoya <shimoyameiji@gmail.com>
2020-03-02 15:39:47 +0900

    どこをクリックしても会話が進むようにした、目的地パネルに縁取りを追加した

--- TaxiNovelUnity/Assets/C#/General/ResolutionStateHolder.cs
+++ TaxiNovelUnity/Assets/C#/General/ResolutionStateHolder.cs
@@ -25,56 +25,61 @@ public static class ResolutionStateHolder
     public static void ResolutionStateChange(ResolutionState state)
     {
         resolutionState = state;
-        
-        Screen.fullScreen = false;
-        Screen.fullScreenMode = FullScreenMode.Windowed;
-        
-        switch (state)
+
+        if (Application.platform == RuntimePlatform.WindowsPlayer)
         {
-            case ResolutionState.FullScreen:
-                Screen.fullScreen = true;
-                Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
-                break;
-                
-            case ResolutionState.FullScreen_Windowed:
-                UniWinApi uniWin = new UniWinApi();
-                var window = UniWinApi.FindWindow();
-                uniWin.SetWindow(window);
-                uniWin.Maximize();
-                uniWin.Dispose();
-                break;
-            
-            case ResolutionState.R3840_2160:
-                Screen.SetResolution(3840, 2160, false);
-                break;
-            
-            case ResolutionState.R2560_1440:
-                Screen.SetResolution(2560, 1440, false);
-                break;
-                
-            case ResolutionStateHolder.ResolutionState.R1920_1080:
-                Screen.SetResolution(1920, 1080, false);
-                break;
-            
-            case ResolutionState.R1600_900:
-                Screen.SetResolution(1600, 900, false);
-                break;
-            
-            case ResolutionState.R1366_768:
-                Screen.SetResolution(1366, 768, false);
-                break;
-            
-            case ResolutionState.R1280_720:
-                Screen.SetResolution(1280, 720, false);
-                break;
-            
-            default:
-                break;
+
+            Screen.fullScreen = false;
+            Screen.fullScreenMode = FullScreenMode.Windowed;
+
+            switch (state)
+            {
+                case ResolutionState.FullScreen:
+                    Screen.fullScreen = true;
+                    Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+                    break;
+
+                case ResolutionState.FullScreen_Windowed:
+                    UniWinApi uniWin = new UniWinApi();
+                    var window = UniWinApi.FindWindow();
+                    uniWin.SetWindow(window);
+                    uniWin.Maximize();
+                    uniWin.Dispose();
+                    break;
+
+                case ResolutionState.R3840_2160:
+                    Screen.SetResolution(3840, 2160, false);
+                    break;
+
+                case ResolutionState.R2560_1440:
+                    Screen.SetResolution(2560, 1440, false);
+                    break;
+
+                case ResolutionState.R1920_1080:
+                    Screen.SetResolution(1920, 1080, false);
+                    break;
+
+                case ResolutionState.R1600_900:
+                    Screen.SetResolution(1600, 900, false);
+                    break;
+
+                case ResolutionState.R1366_768:
+                    Screen.SetResolution(1366, 768, false);
+                    break;
+
+                case ResolutionState.R1280_720:
+                    Screen.SetResolution(1280, 720, false);
+                    break;
+
+                default:
+                    break;
+            }
+
         }
     }
 
     public static void Initialize()
     {
-        ResolutionStateChange(resolutionState);
+        ResolutionStateChange(ResolutionState.FullScreen_Windowed);
     }
 }
