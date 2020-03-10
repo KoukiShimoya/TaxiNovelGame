a1dba69d15a8da57b19dfb876ab5c689fc9c759a
KoukiShimoya <shimoyameiji@gmail.com>
2020-03-10 14:31:27 +0900

    実績導入

---
+++ TaxiNovelUnity/Assets/C#/FungusExtention/SaveSteamStatsFungus.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+namespace Fungus
+{
+    [CommandInfo("Scripting", 
+        "Save Steam Stats", 
+        "Steamの実績を解除する")]
+    [AddComponentMenu("")]
+    public class SaveSteamStatsFungus : Command
+    {
+        [Tooltip("実績の種類")] [SerializeField] private StatsType statsType;
+
+        public override void OnEnter()
+        {
+            SaveSteamStats saveSteamStats = SaveSteamStats.Instance;
+
+            switch (statsType)
+            {
+                case StatsType.End1:
+                    saveSteamStats.Save_End1();
+                    break;
+                case StatsType.End6:
+                    saveSteamStats.Save_End6();
+                    break;
+                case StatsType.End6_3:
+                    saveSteamStats.Save_END6_3();
+                    break;
+                case StatsType.AnyEnd:
+                    saveSteamStats.Save_AnyEnd();
+                    break;
+                default:
+                    break;
+            }
+
+            Continue();
+        }
+
+        public override Color GetButtonColor()
+        {
+            return new Color32(235, 191, 217, 255);
+        }
+        
+        public override string GetSummary()
+        {
+            string summary = statsType.ToString() +"の実績を解除する";
+
+            return summary;
+        }
+
+        private enum StatsType
+        {
+            End1,
+            End6,
+            End6_3,
+            AnyEnd
+        }
+    }
+}
---
+++ TaxiNovelUnity/Assets/C#/Steam/SaveSteamStats.cs
@@ -0,0 +1,59 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+using Steamworks;
+
+public class SaveSteamStats :SingletonMonoBehaviour<SaveSteamStats>
+{
+
+    private const string END1_Stats = "END1_Stats";
+    private const string END6_Stats = "END6_Stats";
+    private const string END6_3_Stats = "End6-3_Stats";
+    private const string AnyEnd_Stats = "AnyEnd_Stats";
+    
+    private void Start() {
+        if(SteamManager.Initialized) {
+            string name = SteamFriends.GetPersonaName();
+            EditorDebug.Log(SteamUtils.GetAppID());
+        }
+    }
+
+    public void Save_End1()
+    {
+        bool isAchieved = false;
+        SteamUserStats.GetAchievement(END1_Stats, out isAchieved);
+        if (!isAchieved)
+        {
+            SteamUserStats.SetAchievement(END1_Stats);
+        }
+    }
+
+    public void Save_End6()
+    {
+        bool isAchieved = false;
+        SteamUserStats.GetAchievement(END6_Stats, out isAchieved);
+        if (!isAchieved)
+        {
+            SteamUserStats.SetAchievement(END6_Stats);
+        }
+    }
+
+    public void Save_END6_3()
+    {
+        bool isAchieved = false;
+        SteamUserStats.GetAchievement(END6_3_Stats, out isAchieved);
+        if (!isAchieved)
+        {
+            SteamUserStats.SetAchievement(END6_3_Stats);
+        }
+    }
+
+    public void Save_AnyEnd()
+    {
+        bool isAchieved = false;
+        SteamUserStats.GetAchievement(AnyEnd_Stats, out isAchieved);
+        if (!isAchieved)
+        {
+            SteamUserStats.SetAchievement(AnyEnd_Stats);
+        }
+    }
+}
