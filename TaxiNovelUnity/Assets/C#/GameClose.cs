7a7c6d155766ae18f8610baa95b7d79b802dd125
KoukiShimoya <shimoyameiji@gmail.com>
2020-03-16 17:49:59 +0900

    ゲームを終了ボタンと、言語切り替えアイコンを追加

---
+++ TaxiNovelUnity/Assets/C#/GameClose.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class GameClose : MonoBehaviour
+{
+    [CanBeNull] [SerializeField] private GameObject checkPanel;
+    
+    public void OnClick_NoCheck()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #elif UNITY_STANDALONE
+            UnityEngine.Application.Quit();
+        #endif
+    }
+
+    public void Onclick_Check()
+    {
+        if (checkPanel == null)
+        {
+            EditorDebug.LogWarning("ゲーム終了の確認パネルがありません");
+        }
+        
+        checkPanel.SetActive(true);
+    }
+
+    public void Onclick_CloseCheckPanel()
+    {
+        if (checkPanel == null)
+        {
+            EditorDebug.LogWarning("ゲーム終了の確認パネルがありません");
+        }
+        
+        checkPanel.SetActive(false);
+    }
+}
